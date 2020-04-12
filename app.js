872ae0df2bd747ac058f5249dcced55f371cb2e0
henry22 <henry811010@gmail.com>
2020-04-12 15:44:34 +0800

    Add url routes

--- app.js
+++ app.js
@@ -15,4 +15,12 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/:url', (req, res) => {
+  res.send('post url')
+})
+
+app.get('/:shorten', (req, res) => {
+  res.send('get shorten url')
+})
+
 app.listen(port, () => console.log(`Server is listening on http://localhost:${port}`))
