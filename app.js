a29e63aeb0ffbb26d8c66c157436ab9c75a14cb1
henry22 <henry811010@gmail.com>
2020-04-12 15:34:26 +0800

    Add index page and style it

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+
 const port = 3000
 
+// serve static files
+app.use(express.static('public'))
+
+// set handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-  res.send('URL shortener')
+  res.render('index')
 })
 
 app.listen(port, () => console.log(`Server is listening on http://localhost:${port}`))
